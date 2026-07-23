f71fc7b517bf5485b8efa7919f114d9a2fa5848b
MariaSalomeGarciaCalle <msgc36375792@soy.sena.edu.co>
2026-07-23 09:44:47 -0500

    Se crea y se sube seccion 4

---
+++ seccion4/Converidorsimple.py
@@ -0,0 +1,3 @@
+kilometros =  float(input("Ingrese la distancia en kilometros: "))
+millas = kilometros * 0.621371
+print("La distancia en millas es:", millas)
---
+++ seccion4/variables.py
@@ -0,0 +1,7 @@
+Nombre = "Salome"
+Edad = 20
+ciudad = "Medellin"
+
+print("Nombre:", Nombre)
+print("Edad:", Edad)
+print("Ciudad:", ciudad)
