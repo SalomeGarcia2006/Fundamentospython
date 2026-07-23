97f6cd16bab5aa297baf765bca5952bdc81afa12
MariaSalomeGarciaCalle <msgc36375792@soy.sena.edu.co>
2026-07-23 09:22:54 -0500

    Se sube proyecto , y carpeta de sccsion 1

---
+++ sesion1/Argumentos.py
@@ -0,0 +1,4 @@
+print ("Pyton", "es un lenguaje de programación")
+print ("Pyton", "es un lenguaje de programación", sep=" _")
+print ("Pyton", end=" _")
+print("3")
---
+++ sesion1/Salida.py
@@ -0,0 +1,3 @@
+print("Nombre:\tSalomé")
+print("Programa:\tADSO")
+print("SENA\nColombia")
